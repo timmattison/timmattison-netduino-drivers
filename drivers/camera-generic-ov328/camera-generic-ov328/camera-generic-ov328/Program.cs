268caa9d39d1f0bad587f4576f3a5cf1b38dd028
timmattison <tim@mattison.org>
2012-03-28 20:35:37 -0400

    Tiny bit of progress

--- drivers/camera-generic-ov328/camera-generic-ov328/camera-generic-ov328/Program.cs
+++ drivers/camera-generic-ov328/camera-generic-ov328/camera-generic-ov328/Program.cs
@@ -6,17 +6,43 @@ using Microsoft.SPOT;
 using Microsoft.SPOT.Hardware;
 using SecretLabs.NETMF.Hardware;
 using SecretLabs.NETMF.Hardware.NetduinoPlus;
+using System.IO.Ports;
 
 namespace camera_generic_ov328
 {
     public class Program
     {
+        static SerialPort serial = null;
+        static byte[] SYNC_COMMAND = { (byte) 0xAA, (byte) 0x0D, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00 };
+
         public static void Main()
         {
-            // write your code here
+            // Open the requested COM port at 9600 bps, no parity, 8 data bits, 1 stop bit
+            serial = new SerialPort(SerialPorts.COM1, 9600, Parity.None, 8, StopBits.One);
+            serial.Open();
+            serial.DataReceived += new SerialDataReceivedEventHandler(serial_DataReceived);
 
-
+            sync();
         }
 
+        private static void serial_DataReceived(Object sender, SerialDataReceivedEventArgs e)
+        {
+            int a = 5;
+            a++;
+        }
+
+        private static void sync()
+        {
+            for (int loop = 0; loop < 59; loop++)
+            {
+                Thread.Sleep(100);
+                write(SYNC_COMMAND);
+            }
+        }
+
+        private static void write(byte[] bytes)
+        {
+            serial.Write(bytes, 0, bytes.Length);
+        }
     }
 }
